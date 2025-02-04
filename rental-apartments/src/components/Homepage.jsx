6358225308b3b5caef801d73a04af7d7bbc90909
Gwendal Boedec <boedec.g@gmail.com>
2025-02-04 19:02:56 +0100

    add homepage component, start basix styling in app.css

---
+++ rental-apartments/src/components/Homepage.jsx
@@ -0,0 +1,26 @@
+function Homepage () {
+return (
+<>
+
+<nav className="navbar"> 
+    <h2>FLAT RENTALS</h2>
+    <img src="./src/img/random-logo.png" alt="logo" />
+     </nav>
+
+<section className="sidebar">
+<li>
+    <ul><a href="">Back to top</a></ul>
+    <ul> <a href="">About</a></ul>
+</li>
+
+</section>
+
+<footer className="footer">
+    <a href="https://github.com/GwendalBoedec/miniproject2/tree/main">Github Project</a>
+    </footer>
+
+</>
+)
+}
+
+export default Homepage
